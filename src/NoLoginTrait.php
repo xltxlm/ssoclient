9587f3f4feed708de3c21ae64072f97496f2a41f
xltxlm <xltxlm@qq.com>
2018-12-28 14:51:32 +0800

    准备更新框架

--- src/NoLoginTrait.php
+++ src/NoLoginTrait.php
@@ -3,17 +3,6 @@
 namespace xltxlm\ssoclient;
 
 use xltxlm\h5skin\Request\UserCookieModel;
-use xltxlm\helper\BasicType;
-use xltxlm\helper\Ctroller\LoadClass;
-use xltxlm\helper\Url\FixUrl;
-use xltxlm\redis\LockKey;
-use xltxlm\redis\RedisCache;
-use xltxlm\redis\RedisGet;
-use xltxlm\ssoclient\Sso\access;
-use xltxlm\ssoclient\Sso\Client\SsoctrolleruserSelectOne;
-use xltxlm\ssoclient\Sso\Ssoctrolleruser;
-use xltxlm\ssoclient\Sso\SsoctrolleruserModel;
-use xltxlm\thrift\Config\ThriftConfig;
 
 /**
  * 登陆检测判断,放在每个需要验证的类入口上面
